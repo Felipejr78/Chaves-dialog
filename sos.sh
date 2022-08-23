ff6842589cec429381d89f5d8c85f88f4abaf581
jorge Felipe <111689286+Felipejr78@users.noreply.github.com>
2022-08-23 15:26:56 -0300

    Update sos.sh

--- sos.sh
+++ sos.sh
@@ -6,17 +6,17 @@
 
 chave=$( dialog --title "sos" --radiolist "Marque as opções desejadas:" 0 0 0 "Instalar chaves" "" off "Iniciar/Recarregar-chaves" "" off "Atualizar chaves" "" off "Importar chaves" "" off "Destravar pacman" "" off --stdout )
 if [ $chave = "Instalar chaves" ];then
-   sudo pacman -Sy archlinux-keyring;clean
-elif [ $chave = "Iniciar/Recarregar-chaves" ];then
-   sudo pacman-key --init;sudo pacman-key --populate archlinux biglinux;sudo pacman -Syy;clean
-elif [ $chave = "Atualizar chaves" ];then
-   sudo pacman-key --refresh-keys;clean
-elif [ $chave = "Importar chaves" ];then
-Import=$( dialog --title "Importar chave" --inputbox "complete o comando" 0 0 "gpg --recv-keys $1 " --stdout )
-    exec $Import 
- fi
- if [ $chave = "Destravar pacman" ];then 
-    sudo rm /var/lib/pacman/db.lck;clean
+      sudo pacman -Sy archlinux-keyring;clean
+   elif [ $chave = "Iniciar/Recarregar-chaves" ];then
+      sudo pacman-key --init;sudo pacman-key --populate archlinux biglinux;sudo pacman -Syy;clean
+   elif [ $chave = "Atualizar chaves" ];then
+      sudo pacman-key --refresh-keys;clean
+   elif [ $chave = "Importar chaves" ];then
+      Import=$( dialog --title "Importar chave" --inputbox "complete o comando" 0 0 "gpg --recv-keys $1 " --stdout )
+      exec $Import 
+fi
+if [ $chave = "Destravar pacman" ];then 
+      sudo rm /var/lib/pacman/db.lck;clean
    
 
 fi     
