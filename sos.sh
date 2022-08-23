1de09122df58c1eec677ead554d55e74061ff354
Jorge Felipe <vbmsjfs@gmail.com>
2022-08-23 15:38:13 -0300

    Atualizando novamente

--- sos.sh
+++ sos.sh
@@ -7,14 +7,14 @@
 chave=$( dialog --title "sos" --radiolist "Marque as opções desejadas:" 0 0 0 "Instalar chaves" "" off "Iniciar/Recarregar-chaves" "" off "Atualizar chaves" "" off "Importar chaves" "" off "Destravar pacman" "" off --stdout )
 if [ $chave = "Instalar chaves" ];then
    sudo pacman -Sy archlinux-keyring;clean
-elif [ $chave = "Iniciar/Recarregar-chaves" ];then
+ elif [ $chave = "Iniciar/Recarregar-chaves" ];then
    sudo pacman-key --init;sudo pacman-key --populate archlinux biglinux;sudo pacman -Syy;clean
-elif [ $chave = "Atualizar chaves" ];then
+ elif [ $chave = "Atualizar chaves" ];then
    sudo pacman-key --refresh-keys;clean
-elif [ $chave = "Importar chaves" ];then
-Import=$( dialog --title "Importar chave" --inputbox "complete o comando" 0 0 "gpg --recv-keys $1 " --stdout )
+ elif [ $chave = "Importar chaves" ];then
+ Import=$( dialog --title "Importar chave" --inputbox "complete o comando" 0 0 "gpg --recv-keys $1 " --stdout )
     exec $Import 
- fi
+fi
  if [ $chave = "Destravar pacman" ];then 
     sudo rm /var/lib/pacman/db.lck;clean
    
@@ -24,4 +24,4 @@ fi
 # Criado por Jorge Felipe dos Santos Júnior
 # Data da criação: 23/08/2022
 # 11:45h
-############################### Nunca é tarde para aprender ################################### 
+############################### Nunca é tarde para aprender !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
